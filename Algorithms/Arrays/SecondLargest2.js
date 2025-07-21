cde9281fcbe6363d26dfa38d12ace785473c3b9e
Aayu8982 <aayu8982@gmail.com>
2025-07-21 22:26:57 +0530

    minor modification on secondlargest question

--- Algorithms/Arrays/SecondLargest2.js
+++ Algorithms/Arrays/SecondLargest2.js
@@ -8,24 +8,28 @@
 
 
 function findSecondLargest2(arr) {
+
+    if(arr.length < 2) {
+        return "Array must contain at least two elements";
+    }
+
     let largest = -Infinity, secondLargest = -Infinity;
 
     for (let i = 0; i < arr.length; i++) {
         if (arr[i] > largest) {
             secondLargest = largest;
             largest = arr[i];
-        } else if (arr[i] < largest && arr[i] > secondLargest) {
+        } else if (arr[i] > secondLargest && arr[i] !== largest) {
             secondLargest = arr[i];
         }
     }
 
-    return secondLargest;
+    return secondLargest !== -Infinity ? secondLargest : "No second largest element found";
 }
 
 
-const arr = [5, 100, 0, -1, 3, 102, 7];
+const arr = [5, 100, 0, -1, 3, 102, -31, 104, 7];
 console.log(findSecondLargest2(arr));
-
 
 // Time Complexity -> O(n)
 // Space Complexity -> O(1)
